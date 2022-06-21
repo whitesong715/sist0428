1f12a3fa6d6a65c648f3891ab1cd5eb45d5d0924
user <user@DESKTOP-COJ9858>
2022-06-21 16:39:46 +0900

    1234

--- MyProject2/src/com/sist/main/MainClass.java
+++ MyProject2/src/com/sist/main/MainClass.java
@@ -5,6 +5,11 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java");
+		
+		System.out.println("Hello Java");
+		
+		
+		
 	}
 
 }
