89c488bdefe783e3bbb6c8222c8b59825b318ed7
user <user@DESKTOP-COJ9858>
2022-06-21 17:18:01 +0900

    1234

--- JDBCBasicProject2/src/com/sist/dao/Emp.java
+++ JDBCBasicProject2/src/com/sist/dao/Emp.java
@@ -5,7 +5,10 @@ package com.sist.dao;
  *   	CHAR(1~2000Byte) => 고정
  *   	CHAR(10)=> aaa => 메모리는 10Byte
  *   	VARCHAR2(1~4000Byte) => 가변
- *   	VARCHAR2(4000)
+ *   	VARCHAR2(4000) => 가변 => 메모리 3byte 가변
+ *      => 한글 바이트 => 3byte
+ *      CLOB => 4기가 => 가변형
+ *      ---------------------자바
  *   숫자형
  *   날짜형
  *   기타형
