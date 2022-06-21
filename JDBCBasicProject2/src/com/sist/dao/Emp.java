56bc6d211d93abcb8d40b8c20f5bf7548868b11f
user <user@DESKTOP-COJ9858>
2022-06-21 17:13:35 +0900

    1222

---
+++ JDBCBasicProject2/src/com/sist/dao/Emp.java
@@ -0,0 +1,15 @@
+package com.sist.dao;
+/*
+ *   오라클 데이터형
+ *   문자형
+ *   	CHAR(1~2000Byte) => 고정
+ *   	CHAR(10)=> aaa => 메모리는 10Byte
+ *   	VARCHAR2(1~4000Byte) => 가변
+ *   	VARCHAR2(4000)
+ *   숫자형
+ *   날짜형
+ *   기타형
+ */
+public class Emp {
+	private int empno;
+}
